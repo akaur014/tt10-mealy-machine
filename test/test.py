ba84b288e5b70543d61d8371d1f5bf5212182b2d
akaur014 <ia0111@wayne.edu>
2026-01-30 00:26:16 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
 
-import cocotb
+import cocotb 
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
